6d9f08268deeb0bcd7c4b250947469557bf8b131
louisbzk <56604782+louisbzk@users.noreply.github.com>
2020-11-07 15:40:12 +0100

    piche

---
+++ TestRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotation : MonoBehaviour
+{
+    public float baseRate = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 eulerRotation = new Vector3(baseRate, 0, 0)*Time.deltaTime;
+        transform.Rotate(eulerRotation);
+    }
+}
---
+++ TextureScrolling.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScrolling : MonoBehaviour
+{
+    public float xScrollSpeed = 0.01f;
+    public float yScrollSpeed = 1f;
+    
+    Renderer renderer;
+    private void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        renderer.material.mainTextureOffset = new Vector2(xScrollSpeed, yScrollSpeed)*Time.time;
+    }
+}
